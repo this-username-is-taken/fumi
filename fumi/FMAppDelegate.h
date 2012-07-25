4e00c9563f3ee9d1de7c99f8fddbd6ba190aba94
wensi <wensi@live.com>
2012-07-24 22:57:48 -0700

    Initial Commit

---
+++ fumi/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  fumi
+//
+//  Created by Vincent Wen on 7/24/12.
+//  Copyright (c) 2012 Vincent Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fumi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fumi
+//
+//  Created by Vincent Wen on 7/24/12.
+//  Copyright (c) 2012 Vincent Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
