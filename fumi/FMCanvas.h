edc796ee746ac8b0dedb42c31c2b3c9859a59f9a
Vincent Wen <wensi@live.com>
2013-02-20 16:42:35 -0500

    Added FMCanvas to divide MVC

---
+++ fumi/FMCanvas.h
@@ -0,0 +1,13 @@
+//
+//  FMCanvas.h
+//  fumi
+//
+//  Created by Vincent Wen on 2/20/13.
+//  Copyright (c) 2013 fumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMCanvas : NSObject
+
+@end
---
+++ fumi/FMCanvas.m
@@ -0,0 +1,13 @@
+//
+//  FMCanvas.m
+//  fumi
+//
+//  Created by Vincent Wen on 2/20/13.
+//  Copyright (c) 2013 fumi. All rights reserved.
+//
+
+#import "FMCanvas.h"
+
+@implementation FMCanvas
+
+@end
